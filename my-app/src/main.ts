c6ccf7763b90706d9bb5b041d4dbe09a08162f26
tke4974652 <tke4974652@gmail.com>
2024-10-29 19:17:21 +0800

    Cart test
    cart implementation attempt

--- my-app/src/main.ts
+++ my-app/src/main.ts
@@ -11,8 +11,9 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
